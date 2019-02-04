d6389a4c1a87118d55bdc3edab040c838bfd0c55
Jacob Walker <jacobisraelw3@gmail.com>
2019-02-04 17:10:13 +0000

    Fixed indentation from tabs to spaces

--- car_calculator.py
+++ car_calculator.py
@@ -9,21 +9,24 @@ gasEfficiency = int(input("Please enter the efficiency of the car in miles per g
 gasResale = int(input("Please enter the estimated resale value of the gas car after 5 years: "))
 preference = input("Do you prefer minimized gas consumption or minimized total cost? ")
 def gasConsumed(gas):
-	return 5*(milesPerYear/gas)
+    return 5*(milesPerYear/gas)
 
 def totalCost(car, gas, resale):
-	total = float(car) + (float(gasConsumed(gas)) * pricePerGallon) - float(resale)
-	return total
+    total = float(car) + (float(gasConsumed(gas)) * pricePerGallon) - float(resale)
+    return total
+
 gasTotal = totalCost(gasInitCost, gasEfficiency, gasResale)
 hybridTotal = totalCost(hybridInitCost, hybridEfficiency, hybridResale)
 
 if preference == "gas":
-	if gasConsumed(gasEfficiency) > gasConsumed(hybridEfficiency):
-		print("The most efficient car is the hybrid.")
-	else: print("The most efficient car is the gas car.")
+    if gasConsumed(gasEfficiency) > gasConsumed(hybridEfficiency):
+        print("The most efficient car is the hybrid.")
+    else:
+        print("The most efficient car is the gas car.")
 else:
-	if gasTotal < hybridTotal:
-		print("The gas car is cheaper, with a total cost of $" + str(round(gasTotal)))
-	else: print("The hybrid car is cheaper, with a total cost of $" + str(round(hybridTotal)))
+    if gasTotal < hybridTotal:
+        print("The gas car is cheaper, with a total cost of $" + str(round(gasTotal)))
+    else:
+        print("The hybrid car is cheaper, with a total cost of $" + str(round(hybridTotal)))
 
 
